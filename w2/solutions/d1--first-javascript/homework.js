6ac220171a6f42cfa39be621dc8523325706a7e6
Clinton Dreisbach <clinton@dreisbach.us>
2018-05-02 22:34:17 -0400

    Running standard

--- w2/solutions/d1--first-javascript/homework.js
+++ w2/solutions/d1--first-javascript/homework.js
@@ -1,43 +1,45 @@
+/* eslint-disable no-unused-vars */
+
 // 1. Set the variable `name` to the string "Addison".
-var givenName = 'Addison';
+var givenName = 'Addison'
 
 // 2. Set candies equal to 20, people to 6, and leftover equal to the remainder of dividing 20 by 6.
-var candies = 20,
-    people = 6,
-    leftover = 20 % 6;
+var candies = 20
+var people = 6
+var leftover = 20 % 6
 
 // 3. Create a function called greeting that returns "Hello, <name>!", where <name> is the name given as an argument to the function
 
-function greeting(name) {
-  return "Hello, " + name + "!"
+function greeting (name) {
+  return 'Hello, ' + name + '!'
 }
 
 // 4. Create a function called isOdd that, given a number, will return true if it is odd and false if it is not.
 
-function isOdd(number) {
-  return (number % 2 === 1 || number % 2 === -1);
+function isOdd (number) {
+  return (number % 2 === 1 || number % 2 === -1)
 }
 
 // 5. Create a function called isEven that, given a number, will return true if it is even and false if it is not.
 
-function isEven(number) {
-  return (number % 2 === 0);
+function isEven (number) {
+  return (number % 2 === 0)
 }
 
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the \
 // temperature in Celsius.
 
-function fahrenheitToCelsius(f) {
-  return (f - 32) * 5 / 9;
+function fahrenheitToCelsius (f) {
+  return (f - 32) * 5 / 9
 }
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the \
 // temperature in Fahrenheit.
 
-function celsiusToFahrenheit(c) {
-  return c * 9 / 5 + 32;
+function celsiusToFahrenheit (c) {
+  return c * 9 / 5 + 32
 }
 
 // 7. Create a function called fahrenheitToKelvin that takes a
@@ -47,7 +49,7 @@ function celsiusToFahrenheit(c) {
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
 
-function fahrenheitToKelvin(f) {
+function fahrenheitToKelvin (f) {
   return fahrenheitToCelsius(f) + 273.15
 }
 
@@ -55,11 +57,11 @@ function fahrenheitToKelvin(f) {
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
 
-function lesser(x, y) {
+function lesser (x, y) {
   if (x < y) {
-    return x;
+    return x
   } else {
-    return y;
+    return y
   }
 }
 
@@ -75,15 +77,15 @@ function lesser(x, y) {
 //
 // If any other language code is used, return nothing.
 
-function multigreeting(name, lang) {
-  if (lang == "en") {
-    return "Hello, " + name + "!";
-  } else if (lang == "es") {
-    return "¡Hola, " + name + "!";
-  } else if (lang == "eo") {
-    return "Saluton, " + name + "!";
-  } else if (lang == "fr") {
-    return "Bonjour, " + name + "!";
+function multigreeting (name, lang) {
+  if (lang === 'en') {
+    return 'Hello, ' + name + '!'
+  } else if (lang === 'es') {
+    return '¡Hola, ' + name + '!'
+  } else if (lang === 'eo') {
+    return 'Saluton, ' + name + '!'
+  } else if (lang === 'fr') {
+    return 'Bonjour, ' + name + '!'
   }
 }
 
@@ -93,13 +95,14 @@ function multigreeting(name, lang) {
 // Date objects.
 // If the birth date is after than the current date, return nothing.
 
-function howOld(birthDate, currentDate) {
+function howOld (birthDate, currentDate) {
   if (birthDate > currentDate) { return }
-  var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  var age = currentDate.getFullYear() - birthDate.getFullYear()
   if (currentDate.getMonth() < birthDate.getMonth() ||
-      (currentDate.getMonth() == birthDate.getMonth() &&
+      (currentDate.getMonth() === birthDate.getMonth() &&
        currentDate.getDay() < birthDate.getDay())) {
-    age -= 1;
+    age -= 1
   }
-  return age;
+  return age
 }
